7fef46e505143eab22d022511b3d5605dc16c19a
Amina Ramazanova <amina_ramazanova@yahoo.com>
2019-04-11 09:24:02 +0300

    Update conanfile.py

--- conanfile.py
+++ conanfile.py
@@ -5,10 +5,11 @@ class SnmpConan(ConanFile):
     name = "net-snmp"
     version = "5.8"
     license = "http://net-snmp.sourceforge.net/about/license.html"
-    author = "Amina Ramazanova"
+    url = "https://sourceforge.net/projects/net-snmp/files/net-snmp/5.8/net-snmp-5.8.tar.gz"
+    author = "InSAT"
     description = "net-snmp package"
     topics = ("net-snmp", "snmp")
-    settings = "os_build", "arch_build", "arch", "compiler"
+    settings = "os", "arch", "compiler"
     options = {"shared": [True, False]}
     default_options = {"shared": True}
     requires = "OpenSSL/1.1.1@conan/stable"
@@ -18,31 +19,57 @@ class SnmpConan(ConanFile):
     def configure(self):
         self.options["OpenSSL"].shared = True
 
+    def system_requirements(self):
+        self.global_system_requirements=True
+
     def source(self):
         git = tools.Git(folder="net-snmp")
-        git.clone("https://github.com/konataa/net-snmp.git", "master")
-        tools.patch(patch_file="net-snmp.patch", base_path=os.path.join(os.getcwd(), "net-snmp"))
-    
-    
-    def build(self):
+        git.clone("/home/parallels/net-snmp-5.8", "master")
+        if self.settings.os == "Windows":
+            tools.patch(patch_file="net-snmp.patch", base_path=os.path.join(os.getcwd(), "net-snmp"))
+
+    def linux_build(self):
+        env_build = AutoToolsBuildEnvironment(self)
+        configure = "./configure --with-default-snmp-version=3 \
+        --with-sys-contact=@@no.where --with-sys-location=Unknown \
+        --with-logfile=/var/log/snmpd.log \
+        --with-persistent-directory=/var/net-snmp \
+        --prefix=%s" % (os.path.join(self.source_folder, "net-snmp"))
+
+        with tools.chdir(os.path.join(self.source_folder, "net-snmp")):
+            self.run("chmod 755 configure")
+            self.run(configure)
+            env_build.make()
+            env_build.install()
+
+
+    def windows_build(self):
         os.environ['OPENSSL_INCLUDE_PATH'] = self.deps_cpp_info["OpenSSL"].include_paths[0]
         os.environ['OPENSSL_LIB_PATH'] = self.deps_cpp_info["OpenSSL"].lib_paths[0]
         config_command = "perl build.pl"
         self.output.warn(config_command)
-        self.run_in_src(config_command)
-    
-    def run_in_src(self, command, show_output=False, win_bash=False):
         with tools.chdir(os.path.join(self.source_folder, "net-snmp\\win32")):
-            self.run(command, win_bash=win_bash, output=True)
+            self.run(config_command, win_bash=True)
         self.output.writeln(" ")
+
+    def build(self):
+        if self.settings.os == "Windows":
+            self.windows_build()
+        elif self.settings.os == "Linux":
+            self.linux_build()
+        else:
+            raise Exception("Unsupported operating system: %s" % self.settings.os)
+
 
     def package(self):
         self.copy("*.h", dst="include", src="net-snmp")
-        self.copy("*.lib", dst="lib", keep_path=False)
-        self.copy("*.dll", dst="bin", keep_path=False)
-        self.copy("*.so", dst="lib", keep_path=False)
-        self.copy("*.dylib", dst="lib", keep_path=False)
-        self.copy("*.a", dst="lib", keep_path=False)
+        if self.settings.os == "Windows":
+            self.copy("*.lib", dst="lib", keep_path=False)
+            self.copy("*.dll", dst="bin", keep_path=False)
+        else:
+            self.copy(pattern="*.dylib", dst="lib", keep_path=False)
+            self.copy(pattern="*.so*", dst="lib", keep_path=False)
+
 
     def package_info(self):
         self.cpp_info.libs = ["netsnmp", "netsnmpagent", "netsnmpmibs", "netsnmptrapd"]

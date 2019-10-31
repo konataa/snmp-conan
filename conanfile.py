c6d2d83ecb90ad5f536641ef8aecc876d4b2c77a
Amina Ramazanova <amina_ramazanova@yahoo.com>
2019-10-31 19:41:18 +0300

    Update conanfile.py

--- conanfile.py
+++ conanfile.py
@@ -6,7 +6,7 @@ class SnmpConan(ConanFile):
     version = "5.8"
     license = "http://net-snmp.sourceforge.net/about/license.html"
     url = "https://sourceforge.net/projects/net-snmp/files/net-snmp/5.8/net-snmp-5.8.tar.gz"
-    author = "Amina Ramazanova / InSAT"
+    author = "Amina Ramazanova / InSAT Company"
     description = "net-snmp package"
     topics = ("net-snmp", "snmp")
     scm = {
